8ca11e89bc2ec6ddb9b420d8ddbd4490a224193f
Tyson Van Buren <bebopsanders@gmail.com>
2021-03-08 20:46:55 -0700

    first linked list

---
+++ main.py
@@ -0,0 +1,37 @@
+class node:
+	def __init__(self,data=None):
+		self.data=data
+		self.next=None
+
+class linked_list:
+	def __init__(self):
+		self.head=node()
+
+	# Adds new node containing 'data' to the end of the linked list.
+	def append(self,data):
+		new_node=node(data)
+		cur=self.head
+		while cur.next!=None:
+			cur=cur.next
+		cur.next=new_node
+
+	# Returns the length (integer) of the linked list.
+	def length(self):
+		cur=self.head
+		total=0
+		while cur.next!=None:
+			total+=1
+			cur=cur.next
+		return total
+
+	# Prints out the linked list in traditional Python list format.
+	def display(self):
+		elems=[]
+		cur_node=self.head
+		while cur_node.next!=None:
+			cur_node=cur_node.next
+			elems.append(cur_node.data)
+		print(elems)
+
+my_list = linked_list()
+my_list.display()
